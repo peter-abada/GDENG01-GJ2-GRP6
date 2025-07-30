05e3f88ab34d8742507ca421d7954c7a03cfa96c
Dacanay, Arvin Lawrence Bundalian <arvin.dacanay@gmail.com>
2025-07-30 19:30:02 +0800

    lesns and gamemanager

--- GDENG02 Game Jam 2 GRP6/Assets/GJ2/Scripts/GameManager.cs
+++ GDENG02 Game Jam 2 GRP6/Assets/GJ2/Scripts/GameManager.cs
@@ -39,16 +39,18 @@ public class GameManager : MonoBehaviour
 
         Time.timeScale = 0f; // Optional: pause gameplay
         ShowCursor(true);
-        victoryPanel.SetActive(true);
-        if (victoryVideo != null)
-        {
-            victoryVideo.loopPointReached += OnVictoryVideoEnd;
-            victoryVideo.Play();
-        }
-        else
-        {
-            ReturnToMainMenu();
-        }
+
+        SceneManager.LoadScene("EndCutscene");
+        //victoryPanel.SetActive(true);
+        //if (victoryVideo != null)
+        //{
+        //    victoryVideo.loopPointReached += OnVictoryVideoEnd;
+        //    victoryVideo.Play();
+        //}
+        //else
+        //{
+        //    ReturnToMainMenu();
+        //}
     }
 
     void OnVictoryVideoEnd(VideoPlayer vp)
